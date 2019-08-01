bd61693133b69a54ab51005fa7a3872f9a72c3bf
Maxeem <jmax.posh@gmail.com>
2019-08-01 04:59:08 +0300

    Apply data binding to MainActivity layout

--- kotlin-coroutines-repository/app/src/main/java/com/example/android/kotlincoroutines/main/MainActivity.kt
+++ kotlin-coroutines-repository/app/src/main/java/com/example/android/kotlincoroutines/main/MainActivity.kt
@@ -16,67 +16,42 @@
 
 package com.example.android.kotlincoroutines.main
 
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.constraintlayout.widget.ConstraintLayout
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.View
-import android.widget.ProgressBar
-import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import com.example.android.kotlincoroutines.R
+import com.example.android.kotlincoroutines.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
-/**
- * Show layout.activity_main and setup data binding.
- */
 class MainActivity : AppCompatActivity() {
 
-    /**
-     * Inflate layout.activity_main and setup data binding.
-     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
-
-        val rootLayout: ConstraintLayout = findViewById(R.id.rootLayout)
-        val title: TextView = findViewById(R.id.title)
-        val spinner: ProgressBar = findViewById(R.id.spinner)
+        // Layout binding
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
         // Get MainViewModel by passing a database to the factory
-        val database = getDatabase(this)
-        val repository = TitleRepository(MainNetworkImpl, database.titleDao)
-        val viewModel = ViewModelProviders
-            .of(this, MainViewModel.FACTORY(repository))
-            .get(MainViewModel::class.java)
+        val repository = TitleRepository(MainNetworkImpl, getDatabase(this).titleDao)
+        val model by viewModels<MainViewModel>(factoryProducer = { MainViewModel.FACTORY(repository) })
 
-        // When rootLayout is clicked call onMainViewClicked in ViewModel
-        rootLayout.setOnClickListener {
-            viewModel.onMainViewClicked()
-        }
+        // Tie view with model
+        binding.model = model
+        binding.lifecycleOwner = this
 
-        // update the title when the [MainViewModel.title] changes
-        viewModel.title.observe(this, Observer { value ->
-            value?.let {
-                title.text = it
-            }
-        })
-
-        // show the spinner when [MainViewModel.spinner] is true
-        viewModel.spinner.observe(this, Observer { value ->
-            value?.let { show ->
-                spinner.visibility = if (show) View.VISIBLE else View.GONE
-            }
+        // Show the spinner when [MainViewModel.spinner] is true
+        model.spinner.observe(this, Observer { show ->
+            binding.spinner.visibility = if (show) View.VISIBLE else View.GONE
         })
 
         // Show a snackbar whenever the [ViewModel.snackbar] is updated with a non-null value
-        viewModel.snackbar.observe(this, Observer { text ->
-            text?.let {
-                Snackbar.make(rootLayout, text, Snackbar.LENGTH_SHORT).show()
-                viewModel.onSnackbarShown()
-            }
-
+        model.snackbar.observe(this, Observer { text ->
+            Snackbar.make(binding.root, text, Snackbar.LENGTH_SHORT).show()
+            model.onSnackbarShown()
         })
     }
+
 }
